135a277914746858e6b877ac9906c600f0a335ff
J. Dobos <joni@MacBook-Pro-van-J.local>
2023-10-20 19:20:56 +0200

    jonimac

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: joni <joni@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/20 11:54:46 by joni          #+#    #+#                 */
-/*   Updated: 2023/10/20 11:56:36 by joni          ########   odam.nl         */
+/*   Updated: 2023/10/20 19:20:30 by joni          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	**head;
+	t_list	*temp;
+
+	temp = *head;
+	if (lst == NULL)
+		return (NULL);
+	temp = ft_lstnew(lst->content);
+	f(temp->content);
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+		ft_lstadd_back(head, ft_lstnew(lst->content));
+		temp = temp->next;
+		f(temp->content);
+	}
+	return (*head);
 }
 
 /*TEST IT*/
