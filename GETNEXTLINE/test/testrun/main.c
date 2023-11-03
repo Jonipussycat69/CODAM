b1ab5569d1051f278eaeaa1a43d5641ef879ebcd
Jonatan Dobos <jdobos@f1r1s14.codam.nl>
2023-11-03 17:44:06 +0100

    codam

--- GETNEXTLINE/test/testrun/main.c
+++ GETNEXTLINE/test/testrun/main.c
@@ -6,8 +6,10 @@ int	main(void)
 	int	fd = open("test.txt", O_RDONLY);
 	if (fd < 0)
 		return (1);
+	printf("Running\n");//CHECK
 	if ((line = get_next_line(fd)) != NULL)
 		printf("next line: %s\n", line);
+	printf("Cleaning\n");//CHECK
 	free(line);
 	close(fd);
 	return (0);
--- GETNEXTLINE/test/testrun/test4l.c
+++ GETNEXTLINE/test/testrun/test4l.c
@@ -26,15 +26,15 @@ void	*lst_free(t_list **head)
 	t_list	*temp;
 	t_list	*temp2;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 	temp = *head;
 	while (temp != NULL)
 	{
-		if (temp->buf)
-			free(temp->buf);
 		temp2 = temp;
 		temp = temp->next;
+		if (temp2->buf)
+			free(temp->buf);
 		free(temp2);
 	}
 	*head = NULL;
@@ -72,19 +72,22 @@ t_list	*node_new(char *buffer)
 {
 	t_list	*new_node;
 	char	*str;
+	char	*tempstr;
 
 	str = (char *)cal(sizeof(char), BUFFER_SIZE + 1);
 	if (!(str))
 		return (NULL);
+	tempstr = str;
 	while (*buffer)
 		*(str++) = *(buffer++);
+	printf("GNL >> node new str >> %s <<\n", tempstr);
 	new_node = (t_list *)malloc(sizeof(t_list));
 	if (!(new_node))
 	{
-		free(str);
+		free(tempstr);
 		return (NULL);
 	}
-	new_node->buf = str;
+	new_node->buf = tempstr;
 	new_node->next = NULL;
 	return (new_node);
 }
@@ -112,19 +115,21 @@ int	read_line(int fd, t_list **head)
 	char	buffer[BUFFER_SIZE + 1];
 	t_list	*new_node;
 	ssize_t	bytesread;
-	int		newline;
 
-	newline = 0;
-	while (bytesread > 0 && newline == 0)
+	printf("GNL >> Running read loop\n");//CHECK
+	bytesread = 1;
+	while (bytesread > 0)
 	{
 		bytesread = read(fd, buffer, BUFFER_SIZE);
+		printf("GNL >> Running read loop >>\n");//CHECK
+		printf("GNL >> Running read loop >> btsr: %zu\n", bytesread);//CHECK
 		if (bytesread <= 0)
 		{
 			if (bytesread < 0)
 				return (-1);
 			break ;
 		}
-		buffer[BUFFER_SIZE] = '\0';
+		buffer[bytesread] = '\0';
 		if (node_add_back(head, node_new(buffer)) == 0)
 			return (-1);
 		if (slen(buffer) != BUFFER_SIZE + 1)
@@ -137,6 +142,7 @@ char	*remain_cpy(char *dest, char *src)
 {
 	size_t	i;
 
+	i = 0;
 	while (dest[i])
 		dest[i++] = '\0';
 	if (slen(src) < BUFFER_SIZE + 1)
@@ -151,6 +157,7 @@ char	*make_str(t_list **head, int lblen)
 	char		*line;
 	t_list		*temp;
 	size_t		ib;
+	size_t		il;
 	static char	remainder[BUFFER_SIZE];
 	
 	line = (char *)cal(((llen(*head) - 1) * BUFFER_SIZE)
@@ -159,14 +166,22 @@ char	*make_str(t_list **head, int lblen)
 		return (NULL);
 	temp = *head;
 	ib = 0;
+	il = 0;
 	line = remain_cpy(line, remainder);
+	printf("GNL >> Running make loop\n");//CHECK
 	while (temp != NULL)
 	{
+		printf("GNL >> Running make loop >>\n");//CHECK
 		while ((temp->buf)[ib] && (temp->buf)[ib] != '\n')
-			*(line++) = (temp->buf)[ib++];
+			line[il++] = (temp->buf)[ib++];
 		ib = 0;
 		if (temp->next == NULL)
-			remain_cpy(temp->buf, remainder);
+		{
+			remain_cpy(remainder, temp->buf);
+			line[il] = '\n';
+		}
+		printf("GNL >> make line >> %s\n", line);//CHECK
+		printf("GNL >> make temp->buf >> %s\n", temp->buf);//CHECK
 		temp = temp->next;
 	}
 	return (line);
@@ -177,11 +192,14 @@ char	*get_next_line(int fd)
 	t_list	*head;
 	int		last_buf_len;
 	char	*line;
-	
+
 	head = NULL;
+	printf("GNL >> Running read\n");//CHECK
 	last_buf_len = read_line(fd, &head);
 	if (last_buf_len < 0)
 		return (lst_free(&head));
+	printf("GNL >> head->buf >> %s\n", head->buf);//CHECK
+	printf("GNL >> Running make\n");//CHECK
 	line = make_str(&head, last_buf_len);
 	printf("line = %s", line);
 	printf("line = %s", head->buf);
