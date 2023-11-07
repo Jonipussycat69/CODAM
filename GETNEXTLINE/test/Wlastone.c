580901975121cac80122399b4cc5e3884d1e4a3f
J. Dobos <joni@MacBook-Pro-van-J.local>
2023-11-07 19:20:35 +0100

    jonipc

--- GETNEXTLINE/test/Wlastone.c
+++ GETNEXTLINE/test/Wlastone.c
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Wlastone.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jdobos <jdobos@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 16:52:50 by jdobos            #+#    #+#             */
-/*   Updated: 2023/11/07 18:33:11 by jdobos           ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   Wlastone.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jdobos <jdobos@student.42.fr>                +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/07 16:52:50 by jdobos        #+#    #+#                 */
+/*   Updated: 2023/11/07 19:20:25 by joni          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	*free_line(char *line)
+void	*free_line(char *line, int type)
 {
-	if (line != NULL)
+	if (type == 1)
+	{
+		if (line && line[0] == '\0')
+		{
+			free(line);
+			return (NULL);
+		}
+		return (line);
+	}
+	if (line != NULL && type == 0)
 		free(line);
 	return (NULL);
 }
@@ -51,13 +60,13 @@ char	*get_buf(char *buf, char *line, size_t i, size_t j)
 	temp_line = line;
 	new_line = (char *)malloc(l_len + b_len + 1);
 	if (!new_line)
-		return (free_line(line));
+		return (free_line(line, 0));
 	while (i < l_len)
 		new_line[i++] = *(temp_line++);
 	while (j < b_len)
 		new_line[i++] = buf[j++];
 	new_line[i] = '\0';
-	free_line(line);
+	free_line(line, 0);
 	i = 0;
 	while (buf[j])
 		buf[i++] = buf[j++];
@@ -83,14 +92,13 @@ char	*get_next_line(int fd)
 	{
 		bytesread = read(fd, buffer, BUFFER_SIZE);
 		if (bytesread < 0)
-			return (free_line(line));
+			return (free_line(line, 0));
 		buffer[bytesread] = '\0';
 		line = get_buf(buffer, line, 0, 0);
 	}
+	line = free_line(line, 1);
 	return (line);
 }
-
-// WORKING EXCEPT FOR EMPTY FILES!!
 
 //WORKING!!! NOW TEST!!!
 
