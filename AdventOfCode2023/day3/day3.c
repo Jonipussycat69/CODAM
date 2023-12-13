bd3759fa7eee887377017fb2218e8ce2217e82ff
Jonatan Dobos <jdobos@f1r2s16.codam.nl>
2023-12-13 16:48:52 +0100

    codam

--- AdventOfCode2023/day3/day3.c
+++ AdventOfCode2023/day3/day3.c
@@ -14,16 +14,46 @@ void	my_strcpy(char *src, char *dest)
 	dest[i] = '\0';
 }
 
+int	ft_get_num(char *line, int begin, int end)
+{
+	int	num = 0;
+
+	while (begin < end)
+		num = num * 10 + (line[begin++] - '0');
+	return (num);
+}
+
+int	ft_sym(char *l1, char *l2, char *l3, int begin, int end)
+{
+	int	i = begin - 1;
+
+	while (l1 && l1[i] && i <= end)
+	{
+		if (!(isdigit(l1[i]) || l1[i] == '.' || l1[i] != '\n'))
+			return (ft_get_num(l2, begin, end));
+		i++;
+	}
+	i = begin - 1;
+	while (l3 && l3[i] && i <= end)
+	{
+		if (!(isdigit(l3[i]) || l3[i] == '.' || l3[i] != '\n'))
+			return (ft_get_num(l2, begin, end));
+		i++;
+	}
+	return (0);
+}
+
 int	main(void)
 {
 	int		fd = open("input3.txt", 'r');
 	char	line_1[BUF_SIZE + 1];
 	char	line_2[BUF_SIZE + 1];
 	char	line_3[BUF_SIZE + 1];
-	char	buffer[BUF_SIZE + 1];
-	ssize_t	bytesread = 1;
-	int	index_num = 0;
-	int	index_sym = 0;
+	ssize_t	bytesread = 0;
+	int		i_b = 0;
+	int		i_num = 0;
+	int		i_sym = 0;
+	int		sum = 0;
 	
 	bytesread = read(fd, line_1, BUF_SIZE);
 	if (bytesread < 0)
@@ -38,10 +68,55 @@ int	main(void)
 		return (1);
 	line_3[bytesread] = '\0';
 	printf("%s\n%s\n%s\n", line_1, line_2, line_3);
-	while ()
+	printf("~ here\n");
+	while (line_1[i_num])
 	{
-		while ()
+		if (isdigit(line_1[i_num]))
+		{
+			i_b = i_num;
+			while (line_1[i_num] && isdigit(line_1[i_num]))
+				i_num++;
+			sum += ft_sym(NULL, line_1, line_2, i_b, i_num);
+		}
+		else
+			i_num++;
 	}
+	printf("~ here\n");
+	while (bytesread != 0)
+	{
+		i_num = 0;
+		while (line_2[i_num])
+		{
+			if (isdigit(line_2[i_num]))
+			{
+				i_b = i_num;
+				while (isdigit(line_2[i_num]))
+					i_num++;
+				sum += ft_sym(line_1, line_2, line_3, i_b, i_num);
+			}
+			i_num++;
+		}
+		my_strcpy(line_2, line_1);
+		my_strcpy(line_3, line_2);
+		bytesread = read(fd, line_3, BUF_SIZE);
+		if (bytesread < 0)
+			return (1);
+		line_3[bytesread] = '\0';
+	}
+	i_num = 0;
+	while (line_2[i_num])
+	{
+		if (isdigit(line_2[i_num]))
+		{
+			i_b = i_num;
+			while (isdigit(line_2[i_num]))
+				i_num++;
+			sum += ft_sym(line_1, line_2, NULL, i_b, i_num);
+		}
+		i_num++;
+	}
+	printf(" line 1: %s\n line 2: %s\n line 3: %s\n", line_1, line_2, line_3);
+	printf("SUM: %d\n", sum);
 	close(fd);
 	return (0);
-}
+} // ANSWER TOO LOW!!!!!!!!!!!!!!
