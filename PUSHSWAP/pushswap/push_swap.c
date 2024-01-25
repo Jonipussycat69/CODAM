ff62d8b85b6a42d8af1d02cabeba6252ec7c7182
Jonatan Dobos <jdobos@f1r2s18.codam.nl>
2024-01-25 17:06:53 +0100

    codam

---
+++ PUSHSWAP/pushswap/push_swap.c
@@ -0,0 +1,34 @@
+#include "push_swap.h"
+
+void	parser(char *input, t_list **head)
+{
+	t_list	*node;
+	int		i;
+	int		num;
+
+	i = 0;
+	while (input[i])
+	{
+		while (input[i] <= '9' && input[i] >= '0')
+		{
+			//LEFTOFF save int here to variable
+		}
+	}
+	ft_lstnew()
+	return ;
+}
+
+void	ft_pushswap(t_list **head)
+{
+	return ;
+}
+
+int	main(void)
+{
+	char	input[] = "12 23 34 45 56 67 78";
+	t_list	**head;
+
+	parser(input, head);
+	ft_pushswap(head);
+	return (0);
+}
