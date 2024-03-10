e43cdc6c93e37a4ed2183af338add9c6b6252e94
J. Dobos <jntndbs@gmail.com>
2024-03-10 09:23:03 +0100

    jonimac

--- PUSHSWAP/pushswap/algo_swap_push.c
+++ PUSHSWAP/pushswap/algo_swap_push.c
@@ -1,48 +1,54 @@
 #include "push_swap.h"
 
-// First Second Last check: Checks which of the 3 is the biggest
-static char	*fsl_check(t_list **head)
+
+int	*get_fsl(t_list **head)
 {
-	static char	ret[4];
-	int			fsl[4];
-	int			i;
-	int			j;
-	int			k;
+	static int	fsl[4];
 
 	fsl[0] = get_index(nth_node(head, 0));
 	fsl[1] = get_index(nth_node(head, 1));
 	fsl[2] = get_index(last_node(head));
 	fsl[3] = 0;
+	return (fsl);
+}
+// First Second Last check: Checks which of the 3 is the biggest
+char	*fsl_order(t_list **head)
+{
+	static char	ret[4];
+	int			*fsl;
+	int			i;
+	int			j;
+	int			place;
 
 	i = 0;
-	k = 0;
+	fsl = get_fsl(head);
 	while (i < 4)
 	{
+		place = 0;
 		j = 0;
 		while (j < 4)
 		{
 			if (fsl[i] < fsl[j])
-				break ;
+				place++;
 			j++;
 		}
-		if (j == 4)
-		{
-			fsl[i] = 0;
-			ret[k] = i;
-			k++;
-		}
+		ret[place] = i;
 		i++;
 	}
+	ret[3] = '\0';
 	return (ret);
 }
-// Determines the actions based on fsl_check, so the smallest gets pushed to b and b is as (invertedly) sorted as possible
+// Determines the actions based on fsl_order, the smallest gets pushed to b and b is as (invertedly) sorted as possible
 short	first_stage(t_list **head_a, t_list **head_b, t_sort *s, short arr)
 {
-	// LEFTOFF
-	const char	*ret_fsl_a = fsl_check(head_b);
-	const char	*ret_fsl_b = fsl_check(head_b);
+	const char	*fsl_a = fsl_order(head_b);
+	const char	*fsl_b = fsl_order(head_b);
 
 	// Here write all the possibible combinations
+	if (fsl_a[0] == fsl_b[0] && fsl_a[1] == fsl_b[1] && fsl_a[2] == fsl_b[2])
+	{
+	// LEFTOFF
+	}
 	return (ok);
 }
 
@@ -62,5 +68,5 @@ short	algo_swap_push(t_list **head_a, t_list **head_b, t_sort *s, short arr)
 	// stage 2: marking values in b with the revious value done in main
 	// stage 3: pushing from b to a
 	
-	return ;
+	return (ok);
 }
