98ae581c8396123cbb58d252b0d98536e7390381
J. Dobos <jntndbs@gmail.com>
2024-04-03 13:27:24 +0200

    jonimac

--- PUSHSWAP/BEST/utils_sort.c
+++ PUSHSWAP/BEST/utils_sort.c
@@ -13,7 +13,27 @@ void	act_arr_reset(t_sort *s)
 	return ;
 }
 
+int	rotate_b(t_list **b, int num)
+{
+	t_list	*tmp;
+	int		i;
+
+	i = 0;
+	tmp = *b;
+	while (tmp->num > num && tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
+
 void	weigh(t_list **a, t_list **b, t_sort *s, t_list *node)
 {
+	const	r_a = s->index;
+	const	rr_a = s->index - list_len(a);
+	const	r_b = rotate_b(b, node->num);
+	const	rr_b = r_b - list_len(b);
+	
 	// LEFTOFF
 }
