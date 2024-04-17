7ca6bff92bba5efeef89a28d8e1d6a80df5732c9
J. Dobos <jntndbs@gmail.com>
2024-04-17 11:43:26 +0200

    jonim

--- FRACTOL/lib/fractol.h
+++ FRACTOL/lib/fractol.h
@@ -7,8 +7,31 @@
 #include "../libft/libft.h"
 #include "MLX42/MLX42.h"
 
-# define WIDTH 600
-# define HEIGHT 500
+# define WIDTH 800
+# define HEIGHT 600
+
+enum	sets
+{
+	mandel = 1,
+	julia = 2
+};
+
+typedef struct complex_s
+{
+	double	real;
+	double	imag;
+}	c_t;
+
+typedef struct fractol_s
+{
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+	size_t		iterations;
+	c_t			*c;
+	short		set;
+	double		inp;
+}	fra_t;
+
 
 
 #endif
--- FRACTOL/src/main.c
+++ FRACTOL/src/main.c
@@ -7,35 +7,78 @@ static void ft_error(void)
 	exit(EXIT_FAILURE);
 }
 
-// Print the window width and height.
-static void ft_hook(void* param)
+void	ft_window_size_reset(fra_t *f)
 {
-	const mlx_t	*mlx = param;
-
-	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+	mlx_resize_image(f->img, WIDTH, HEIGHT);
+	mlx_set_window_size(f->mlx, WIDTH, HEIGHT);
 }
 
-void	window(char *name)
+void	ft_key(mlx_key_data_t keydata, void* param)
+{
+	fra_t	*f;
+	
+	f = param;
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+	{
+		mlx_terminate(f->mlx);
+		exit(EXIT_SUCCESS);
+	}
+	if (keydata.key == MLX_KEY_R && keydata.action == MLX_PRESS)
+		ft_window_size_reset(f);
+}
+
+void	draw(int32_t width, int32_t height, const fra_t *f)
+{
+	int	w;
+	int	h;
+	
+	w = 0;
+	while (w < width)
+	{
+		h = 0;
+		while (h < height)
+		{
+			if (h < height / 2)
+				mlx_put_pixel(f->img, w, h, 0x0000FFFF);
+			else
+				mlx_put_pixel(f->img, w, h, 0xFF0000FF);
+			h++;
+		}
+		w++;
+	}
+}
+
+static void ft_image_redraw(int32_t width, int32_t height, void* param)
+{
+	const fra_t	*f = param;
+
+	printf("WIDTH: %d | HEIGHT: %d\n", width, height);// TEST
+	mlx_resize_image(f->img, width, height);
+	draw(width, height, f);
+}
+
+void	window(char *name, fra_t *f)
 {
 	mlx_set_setting(MLX_MAXIMIZED, false);
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, name, true);
-	if (!mlx)
+	f->mlx = mlx_init(WIDTH, HEIGHT, name, true);
+	if (!f->mlx)
 		ft_error();
-	
-	mlx_image_t* img = mlx_new_image(mlx, 100, 100);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+	f->img = mlx_new_image(f->mlx, WIDTH, HEIGHT);
+	if (!f->img || (mlx_image_to_window(f->mlx, f->img, 0, 0) < 0))
 		ft_error();
-	
-	mlx_put_pixel(img, 0, 0, 0xFFFFFFFF);
-	mlx_loop_hook(mlx, ft_hook, mlx);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
+	draw(WIDTH, HEIGHT, f);
+	mlx_key_hook(f->mlx, ft_key, f);
+	mlx_resize_hook(f->mlx, ft_image_redraw, f);
+	mlx_loop(f->mlx);
+	mlx_terminate(f->mlx);
 }
 
 int32_t	main(int argc, char **argv)
 {
+	fra_t	f;
+
 	if (argc != 2)
 		ft_error();
-	window(argv[1]);
+	window(argv[1], &f);
 	return (EXIT_SUCCESS);
 }
