50708dcf87ddb089d81cb406297d6e59bed667de
J. Dobos <jntndbs@gmail.com>
2024-04-17 08:47:57 +0200

    joinmac

---
+++ FRACTOL/lib/fractol.h
@@ -0,0 +1,14 @@
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "../libft/libft.h"
+#include "MLX42/MLX42.h"
+
+# define WIDTH 600
+# define HEIGHT 500
+
+
+#endif
--- FRACTOL/src/main.c
+++ FRACTOL/src/main.c
@@ -1,11 +1,4 @@
-// Written by Bruh
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include "MLX42/MLX42.h"
-#define WIDTH 256
-#define HEIGHT 256
+#include "../lib/fractol.h"
 
 // Exit the program as failure.
 static void ft_error(void)
@@ -17,34 +10,32 @@ static void ft_error(void)
 // Print the window width and height.
 static void ft_hook(void* param)
 {
-	const mlx_t* mlx = param;
+	const mlx_t	*mlx = param;
 
 	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
 }
 
-int32_t	main(void)
+void	window(char *name)
 {
-
-	// MLX allows you to define its core behaviour before startup.
-	mlx_set_setting(MLX_MAXIMIZED, true);
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
+	mlx_set_setting(MLX_MAXIMIZED, false);
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, name, true);
 	if (!mlx)
 		ft_error();
-
-	/* Do stuff */
-
-	// Create and display the image.
-	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+	
+	mlx_image_t* img = mlx_new_image(mlx, 100, 100);
 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
 		ft_error();
-
-	// Even after the image is being displayed, we can still modify the buffer.
-	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
-
-	// Register a hook and pass mlx as an optional param.
-	// NOTE: Do this before calling mlx_loop!
+	
+	mlx_put_pixel(img, 0, 0, 0xFFFFFFFF);
 	mlx_loop_hook(mlx, ft_hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
+}
+
+int32_t	main(int argc, char **argv)
+{
+	if (argc != 2)
+		ft_error();
+	window(argv[1]);
 	return (EXIT_SUCCESS);
 }
