8b3de89492adbe0a1dea9ea3ced1510f814cb359
joni <jntndbs@gmail.com>
2024-06-12 22:30:51 +0200

    u

--- MINISHELL/FIRST/minishell.h
+++ MINISHELL/FIRST/minishell.h
@@ -1,9 +1,12 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
 # include <stdio.h>
 # include <unistd.h>
 # include <string.h>
 # include <stdlib.h>
+# include <readline.h>
 
 
 
---
+++ MINISHELL/FIRST/src/main.c
@@ -0,0 +1,16 @@
+#include "../minishell.h"
+
+void	the_loop(void)
+{
+	while (1)
+	{
+		readline()
+	}
+}
+
+int	main(void)
+{
+	ft_printf("START\n");
+	the_loop();
+	ft_printf("END\n");
+}
