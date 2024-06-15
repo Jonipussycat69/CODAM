496decd4bd462234d327b143798075975cd42485
joni <jntndbs@gmail.com>
2024-06-15 22:37:30 +0200

    u

--- MINISHELL/minishell.h
+++ MINISHELL/minishell.h
@@ -14,7 +14,19 @@
 # include <fcntl.h>
 # include <stdbool.h>
 
+typedef struct s_cmd_node
+{
+	char	*content;
+	void	*next;
+}	t_cmd_node;
 
+typedef struct s_sig_argument
+{
+	t_cmd_node	**cmd_head; // can be used as single pointer, head stored in sig_argument node only
+	char		**cmd_array;
+	int			token;
+	void		*next;
+}	t_sig_argument;
 
 
 #endif
